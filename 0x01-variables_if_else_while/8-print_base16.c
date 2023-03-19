29deabb16503c808d76ea0b4b68cdefa8f1cd9e3
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-20 02:41:22 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 for success exe
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
