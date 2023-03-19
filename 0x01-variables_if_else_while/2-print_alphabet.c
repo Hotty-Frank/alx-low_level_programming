8cfdb3810aebb7003b5b42fc3872ea032e5af227
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-19 22:29:04 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0  for success
+ */
+int main()
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+	{
+		putchar(n);
+	}
+	return (0);
+}
