06fdb9e263d31a197ebc8d70b284e5302d2a34ef
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-20 02:00:49 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alaways 0 for succes
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 1);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
