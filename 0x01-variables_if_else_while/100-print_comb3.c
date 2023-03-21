bf96ea3fe7fb73a1c70510fbeab2fa737f1bcef5
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-22 02:13:55 +0300

    2-print_alphabet.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 /**
  * main- Entry point
- * Return: Alwaays 0 for success
+ * Return: Always 0 for success
  */
 int main(void)
 {
 	int n, m;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; m < 58; n++)
 		for (m = 49; m < 58; m++)
 		{
 			{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+				putchar(n);
+				putchar(m);
+				n++;
+				m++;
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	{
