3bbd18f038713e909051872f9b11d16752006917
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-21 23:41:37 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
