8909b470cff522f0ed5d5f641f3d9e889753e493
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-22 15:29:24 +0300

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - a function that checks all upper and lowercase alphabets.
+ *@c: Function parameters
+ *Return: 1 if alphabet letter else Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91)
+		if (c > 96 && c < 123)
+		{
+			return (1);
+		}
+	return (0);
+}
+
