039abd2e966b57686876b173c4e74396551f3eee
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-22 23:24:45 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - Function that prints absolute vales od integers
+ *@i: Function parameters..
+ *Return: absolute vaue of int ans appropriate type.
+ */
+int _abs(int i)
+{
+	if (i == 0 && i > 0)
+	{
+		_putchar(i);
+	}
+	return (-i);
+}
