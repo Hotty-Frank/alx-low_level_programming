efce98e79ed4b600b4018a0e952b984cb0d51ad3
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-23 16:27:03 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - function that check positive 0-9 numbers
+ * @c: function parameters.
+ * Return: 1 if its a digit otherwise return 0
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	putchar('\n');
+	return (0);
+}
