84822f6f38092723c4b4e1af9eedbffb2b6f045e
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-25 01:39:06 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,31 @@ int main(void)
 	int a;
 
 	for (a = 1; a <= 100; a++)
+	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz");
+			if (a == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
-			printf("%d", a);
+			printf("%d ", a);
 		}
-	putchar('\n');
+	}
+	printf("\n");
 	return (0);
 }
