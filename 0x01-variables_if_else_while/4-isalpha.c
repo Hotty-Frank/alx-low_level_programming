dc75d347493c965d5acd5cfd5e308178acebf1a9
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-27 15:47:27 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha Function is used to check for all alphabets
+ * @c: parameters of the function.
+ * Return: 1 if c is an alphabet else Return: 0.
+ */
+int _isalpha(int c)#include
+{
+	if (c > 64 && c > 91)
+	:	OR
+			if (c > 96 && c > 123)
+	{
+		return (1);
+	}
+	return (0);
+}
