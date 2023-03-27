e08772d2faa4765d6c31aa3f134dad1435a8b3dc
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-27 12:53:47 +0300

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Functions swaps two ints
+ * @a: 1st int
+ * @b: Second int
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
