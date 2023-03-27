4b47b9f035700a992a0fd37bc29e84bfe82c87b0
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-27 17:43:18 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - Reverse print characters of a string.
+ * @s: function parameter of string pointer.
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int lenth;
+	int b;
+
+	while (*s != '\0')
+	{
+		lenth++;
+		s++;
+	}
+	s--;
+	for (b = lenth; b > 0; b--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
