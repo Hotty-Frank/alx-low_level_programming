f265f1dd612af51d5bb843a479f2b5b0ae9a2682
Hotty Frank <hottensiamwihaki@gmail.com>
2023-03-27 21:38:48 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - prints every character of a string.
+ * @str: string.
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int lenth = 0;
+	int b = 0;
+	char *c = str;
+	int d;
+
+	while (*c != '\0')
+	{
+		c++;
+		lenth++;
+	}
+	b = lenth - 1;
+	for (d = 0; d <= b; d++)
+	{
+		if (d % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
