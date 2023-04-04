23b31fd935bd93caa26fa9ea6423c570afbaecbe
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-04 04:31:08 +0300

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of 2 square diagnals.
+ * @a: fuction parameter
+ * @size: size parameter
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum, sum1, b;
+
+	sum = 0;
+	sum1 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		sum = sum + a[b * size + b];
+	}
+
+	for (b = size - 1; b >= 0; b--)
+	{
+		sum1 += a[b * size + (size - b - 1)];
+	}
+
+	printf("%d, %d\n", sum, sum1);
+}
+
