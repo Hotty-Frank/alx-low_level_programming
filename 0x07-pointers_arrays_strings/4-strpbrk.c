842871e8cea7f1782e9999ac99fc34e3f08b2902
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-04 03:50:46 +0300

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,4 +8,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
+
 
