db39ccd174f9c60281519f72bb3063e690ffcc7c
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-10 11:30:37 +0300

    equate argc and argv values

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function body in c language.
+ * @argc: arguement count.
+ * @argv: argument vector.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
