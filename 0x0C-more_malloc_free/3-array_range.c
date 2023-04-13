e9877a2b2f30fe4dbc1dba0b8d0ed2e5a4700c6d
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-13 23:54:30 +0300

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * array_range - creates and array with all values of parameter.
+ * @min:funtion parameter of array
+ * @max: function parameter
+ * Return: pointer to the new string or null.
+ */
+int *array_range(int min, int max)
+{
+	int *new;
+
+	int x;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(int) * (max - min) + sizeof(int));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	while (min <= max)
+	{
+		new[x] = min;
+		x++;
+		min++;
+	}
+	return (new);
+}
