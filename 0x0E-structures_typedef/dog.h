9b8bc61eefeaedaf4f2c49127aeda2e13b3dc213
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-18 00:41:55 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - functions with structure info type dog.
+ * @name: the name
+ * @age: the age
+ * @owner: who owns the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
