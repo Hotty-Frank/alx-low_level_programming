ba20caab0645e4684753066d4c2d2f14ca87bdab
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-20 01:15:39 +0300

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - body functio prints opcodes
+ * @argc: arguements count.
+ * @argv: arguement vector
+ * Return: Always zero.
+ */
+int main(int argc, char *argv[])
+{
+	int byts, k;
+	char *ar;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	byts = atoi(argv[1]);
+
+	if (byts < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ar = (char *)main;
+	for (k = 0; k < byts; k++)
+	{
+		if (k == byts - 1)
+		{
+			printf("%02hhx\n", ar[k]);
+			break;
+		}
+		printf("%02hhx\n ", ar[k]);
+	}
+	return (0);
+}
+
